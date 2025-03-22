7f8535ec9db1529e583a2eea3b0371078da1c8c7
andyBzg <andr.bzg@gmail.com>
2025-03-22 11:45:17 +0100

    add capture network devices

--- NetworkSniffer/Program.cs
+++ NetworkSniffer/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace NetworkSniffer
+﻿using SharpPcap;
+
+namespace NetworkSniffer
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Display all network devices
+            var devices = CaptureDeviceList.Instance;
+            if (devices.Count < 1)
+            {
+                Console.WriteLine("No network devices found.");
+                return;
+            }
+
+            Console.WriteLine("Select a device to capture packets:");
+            int i = 0;
+            foreach (var dev in devices)
+            {
+                Console.WriteLine($"[{i}] {dev.Description}");
+                i++;
+            }
         }
     }
 }
