1f0ae1586937f3a54841226b305b38aa48b5ad1c
andyBzg <andr.bzg@gmail.com>
2025-03-28 12:00:22 +0100

    add unit tests for dhcp and http handlers

--- NetworkSniffer.Tests/Services/Handlers/HttpPacketHandlerTests.cs
+++ NetworkSniffer.Tests/Services/Handlers/HttpPacketHandlerTests.cs
@@ -2,34 +2,39 @@
 using NetworkSniffer.Interfaces;
 using NetworkSniffer.Services.Handlers;
 using PacketDotNet;
-using PacketDotNet.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace NetworkSniffer.Tests.Services.Handlers
 {
-    public class DhcpPacketHandlerTests
+    public class HttpPacketHandlerTests
     {
         private readonly Mock<ILogger> _mockLogger;
-        private readonly DhcpPacketHandler _handler;
+        private readonly HttpPacketHandler _handler;
         private readonly PhysicalAddress _senderMac;
         private readonly PhysicalAddress _targetMac;
 
-        public DhcpPacketHandlerTests()
+        public HttpPacketHandlerTests()
         {
             _mockLogger = new Mock<ILogger>();
-            _handler = new DhcpPacketHandler(_mockLogger.Object);
+            _handler = new HttpPacketHandler(_mockLogger.Object);
             _senderMac = new PhysicalAddress(new byte[] { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55 });
             _targetMac = new PhysicalAddress(new byte[] { 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF });
         }
 
         [Fact]
-        public void CanHandlePacket_ValidDhcpPacket_ReturnsTrue()
+        public void CanHandlePacket_ValidHttpPacket_ReturnsTrue()
         {
             // Arrange
+            var tcpPacket = new TcpPacket(1234, 80);
+            tcpPacket.PayloadData = Encoding.UTF8.GetBytes("GET / HTTP/1.1");
+            
             var ethernetPacket = new EthernetPacket(_senderMac, _targetMac, EthernetType.IPv4);
-            var rawData = new byte[240];
-            var dhcpV4Packet = new DhcpV4Packet(new ByteArraySegment(rawData), ethernetPacket);
-            ethernetPacket.PayloadPacket = dhcpV4Packet;
+            ethernetPacket.PayloadPacket = tcpPacket;
 
             // Act
             bool result = _handler.CanHandlePacket(ethernetPacket);
@@ -39,12 +44,12 @@ namespace NetworkSniffer.Tests.Services.Handlers
         }
 
         [Fact]
-        public void CanHandlePacket_NonDhcpPacket_ReturnsFalse()
+        public void CanHandlePacket_NonHttpPacket_ReturnsFalse()
         {
             // Arrange
-            var tcpPacket = new TcpPacket(1234, 80);
+            var udpPacket = new UdpPacket(1234, 80);
             var ethernetPacket = new EthernetPacket(_senderMac, _targetMac, EthernetType.IPv4);
-            ethernetPacket.PayloadPacket = tcpPacket;
+            ethernetPacket.PayloadPacket = udpPacket;
 
             // Act
             bool result = _handler.CanHandlePacket(ethernetPacket);
@@ -54,17 +59,14 @@ namespace NetworkSniffer.Tests.Services.Handlers
         }
 
         [Fact]
-        public void HandlePacket_ValidDhcpPacket_LogsCorrectMessage()
+        public void HandlePacket_ValidHttpPacket_LogsCorrectMessage()
         {
             // Arrange
+            var tcpPacket = new TcpPacket(1234, 80);
+            tcpPacket.PayloadData = Encoding.UTF8.GetBytes("GET / HTTP/1.1");
+            
             var ethernetPacket = new EthernetPacket(_senderMac, _targetMac, EthernetType.IPv4);
-            byte[] rawData = new byte[244]; // DHCP header 240 bytes + 4 bytes of padding
-            rawData[0] = 1;  // BOOTP Message Type (1 = Request)
-            rawData[236] = 53; // DHCP Option 53 (DHCP Message Type)
-            rawData[237] = 1;  // Length of DHCP Message Type
-            rawData[238] = (byte)DhcpV4MessageType.Ack; // DHCP Message Type
-            var dhcpV4Packet = new DhcpV4Packet(new ByteArraySegment(rawData), ethernetPacket);
-            ethernetPacket.PayloadPacket = dhcpV4Packet;
+            ethernetPacket.PayloadPacket = tcpPacket;
 
             // Act
             _handler.HandlePacket(ethernetPacket);
@@ -74,10 +76,27 @@ namespace NetworkSniffer.Tests.Services.Handlers
         }
 
         [Fact]
-        public void HandlePacket_NonDhcpPacket_DoesNotLogMessage()
+        public void HandlePacket_NonHttpPacket_DoesNotLogMessage()
+        {
+            // Arrange
+            var udpPacket = new UdpPacket(1234, 80);
+            var ethernetPacket = new EthernetPacket(_senderMac, _targetMac, EthernetType.IPv4);
+            ethernetPacket.PayloadPacket = udpPacket;
+
+            // Act
+            _handler.HandlePacket(ethernetPacket);
+
+            // Assert
+            _mockLogger.Verify(l => l.Log(It.IsAny<string>(), It.IsAny<ConsoleColor>()), Times.Never);
+        }
+
+        [Fact]
+        public void HandlePacket_HttpPacketWithEmptyPayload_DoesNotLogMessage()
         {
             // Arrange
             var tcpPacket = new TcpPacket(1234, 80);
+            tcpPacket.PayloadData = new byte[0];
+
             var ethernetPacket = new EthernetPacket(_senderMac, _targetMac, EthernetType.IPv4);
             ethernetPacket.PayloadPacket = tcpPacket;
 
@@ -89,10 +108,12 @@ namespace NetworkSniffer.Tests.Services.Handlers
         }
 
         [Fact]
-        public void HandlePacket_NullPacket_DoesNotLogMessage()
+        public void HandlePacket_HttpPacketWithNullPayload_DoesNotLogMessage()
         {
             // Arrange
-            EthernetPacket ethernetPacket = new EthernetPacket(_senderMac, _targetMac, EthernetType.IPv4);
+            var tcpPacket = new TcpPacket(1234, 80);
+            var ethernetPacket = new EthernetPacket(_senderMac, _targetMac, EthernetType.IPv4);
+            ethernetPacket.PayloadPacket = tcpPacket;
 
             // Act
             _handler.HandlePacket(ethernetPacket);
